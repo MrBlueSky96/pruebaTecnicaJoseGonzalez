64c8811c4c763f5b34475edd4f036e70113da893
Jose Gonzalez <josegonzalezguilabert@gmail.com>
2021-06-08 08:52:13 +0200

    implementado routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeliculasComponent } from './componentes/peliculas/peliculas.component';
+import { PersonajesComponent } from './componentes/personajes/personajes.component';
+import { PortadaComponent } from './componentes/portada/portada.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PortadaComponent
+  },
+  {
+    path: 'personajes',
+    component: PersonajesComponent
+  },
+  {
+    path: 'peliculas',
+    component: PeliculasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonajesComponent } from './componentes/personajes/personajes.component';
+import { PeliculasComponent } from './componentes/peliculas/peliculas.component';
+import { PortadaComponent } from './componentes/portada/portada.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonajesComponent,
+    PeliculasComponent,
+    PortadaComponent
   ],
   imports: [
     BrowserModule,
